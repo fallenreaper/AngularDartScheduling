1f3c8458365054047bad6fc3ba4d7d9dbfdc3648
William Francis <WFrancis@trustwave.com>
2017-04-26 16:58:42 -0400

    a base view of the scheduler

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -5,12 +5,13 @@ import 'package:angular2/core.dart';
 import 'package:angular2_components/angular2_components.dart';
 
 import 'hello_dialog/hello_dialog.dart';
+import 'scheduler/scheduler.dart';
 
 @Component(
   selector: 'my-app',
   styleUrls: const ['app_component.css'],
   templateUrl: 'app_component.html',
-  directives: const [materialDirectives, HelloDialog],
+  directives: const [materialDirectives, HelloDialog, Scheduler],
   providers: const [materialProviders],
 )
 class AppComponent {
